23eb14d3f80dad5dc10ed82c57b3c72344b2f41a
Faith Magras <bdanielle092@gmail.com>
2020-07-17 11:25:58 -0500

    added files

---
+++ src/components/events/EventCard.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const EventCard = () => {
+    return (
+        <div calssName="card">
+            <div className="card-content">
+                <h3>
+    Event: <span className="card-eventName"></span>
+                </h3>
+                
+            </div>
+        </div>
+    )
+}
+export default EventCard
