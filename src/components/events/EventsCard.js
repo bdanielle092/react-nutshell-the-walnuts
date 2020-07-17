01a2279cc1ee7998ebc3716c1731a967f58045a4
Faith Magras <bdanielle092@gmail.com>
2020-07-17 09:37:32 -0500

    worked on home page

---
+++ src/components/events/EventsCard.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const eventCard = () => {
+    return (
+        <div calssName="card">
+            <div className="card-content">
+                <h3>
+    Event: <span className="card-eventName"></span>
+                </h3>
+            </div>
+        </div>
+    )
+}
